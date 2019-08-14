12ba08e9fc8898d0682ab8472d404a35c0eeeebc
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2019-08-14 20:17:52 -0300

    Initial files, https funcs to get json data from giphy website, and more

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:giphy_finder_app/ui/home.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+

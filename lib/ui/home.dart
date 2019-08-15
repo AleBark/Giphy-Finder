d305dd4d28001ce95efad03ad7b7b07ed4f73166
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2019-08-14 22:03:14 -0300

    showing trending gifs if search term is empty and adding transitions on gifs

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:giphy_finder_app/ui/gif.dart';
 import 'package:http/http.dart' as http;
 import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,7 +18,7 @@ class _HomePageState extends State<HomePage> {
     http.Response response;
 
     //Show trending gifs if no search term specified
-    if (_search == null) {
+    if(_search == null || _search.isEmpty){
       response = await http.get(
           "https://api.giphy.com/v1/gifs/trending?api_key=VLMWD140B8bDDjP0tQpQ8Ft6Mv1Hccue&limit=20&rating=G");
     } else {
@@ -113,11 +114,7 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           if (_search == null || index < snapshot.data["data"].length)
             return GestureDetector(
-              child: Image.network(
-                snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-                height: 300.0,
-                fit: BoxFit.cover,
-              ),
+              child: FadeInImage.memoryNetwork(placeholder: kTransparentImage, image: snapshot.data["data"][index]["images"]["fixed_height"]["url"], height: 300.0, fit: BoxFit.cover,),
               onTap: () {
                 Navigator.push(
                     context,
